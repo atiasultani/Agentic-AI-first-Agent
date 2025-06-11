9e34845674e8f4efd9df775f28947226b712e4a4
atiasultani <160133182+atiasultani@users.noreply.github.com>
2025-06-11 13:19:13 +0500

    done

--- explore-uv/src/explore_uv/main.py
+++ explore-uv/src/explore_uv/main.py
@@ -1,25 +1,34 @@
 from dotenv import load_dotenv
 import os
-from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel
+from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel 
+from agents.run import RunConfig
+import asyncio
 
 # Load environment variables
 load_dotenv()
 
-def main():
+async def main():
     # Get secret key
     secret_key = os.getenv("GEMINI_API_KEY")
 
     # Model and client setup
     MODEL_NAME = "gemini-2.0-flash"
+
     external_client = AsyncOpenAI(
         api_key=secret_key,
         base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
     )
 
+    
     model = OpenAIChatCompletionsModel(
         model=MODEL_NAME,
         openai_client=external_client
     )
+
+    config=RunConfig(
+        model=model,
+        model_provider=external_client,
+        tracing_disabled=True)
 
     # Create the agent
     assistance = Agent(
@@ -29,12 +38,13 @@ def main():
     )
 
     # Run the agent
-    result = Runner.run_sync(
+    result = await Runner.run(
         starting_agent=assistance,
-        input="Hello, how are you?"
+        input="Hello, how are you?",
+        run_config=config
     )
 
     print(result.final_output)
 
 if __name__ == '__main__':
-    main()
+   asyncio.run(main())
